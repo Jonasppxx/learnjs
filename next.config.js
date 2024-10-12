5215ecc5b76ec984e16c613165ce23e455e2b81b
Jonasppxx <Jonas.boos@bwdbern.ch>
2024-10-12 14:00:21 +0200

    v4

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
+  experimental: {
+    runtime: 'edge',
+    serverComponents: true,
+  },
 }
 
 module.exports = nextConfig
