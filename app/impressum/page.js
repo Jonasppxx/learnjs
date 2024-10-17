23eaa98f84892c081547679121cfc1b45067d597
Jonasppxx <Jonas.boos@bwdbern.ch>
2024-10-17 16:53:45 +0200

    v3.3

--- app/impressum/page.js
+++ app/impressum/page.js
@@ -14,7 +14,32 @@ export default function Impressum() {
         </Link>
       </div>
       
-      {/* Der Rest des Inhalts bleibt unverändert */}
+      <section className="mb-6">
+        <h2 className="text-2xl font-semibold mb-3">Persönliche Angaben</h2>
+        <p>Pokebuy</p>
+        <p>Gantrischweg</p>
+        <p>3123 Belp</p>
+        <p>Schweiz</p>
+      </section>
+
+      <section className="mb-6">
+        <h2 className="text-2xl font-semibold mb-3">Kontakt</h2>
+        <p>Telefon: 079 308 19 99</p>
+        <p>E-Mail: pokebuy@gmail.com</p>
+      </section>
+
+      <section className="mb-6">
+        <h2 className="text-2xl font-semibold mb-3">Vertreten durch</h2>
+        <p>Jonas Boos</p>
+      </section>
+
+
+
+
+
+
+
+
     </div>
   );
 }
